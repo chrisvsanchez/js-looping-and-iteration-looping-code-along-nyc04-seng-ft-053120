b82c40a801f95f3a1ba8363ce46d2ef56e68e884
chrisvsanchez <chrisvsanchez@gmail.com>
2020-08-01 19:00:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+
+function writeCards(name){
+   let result = []
+    for (let i = 0; i < name.length; i++){
+        result.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    }
+    return result;
+}
+function countDown(num){
+    for (let i = num; i >= 0; i--){
+        console.log(i);
+    }
+}
